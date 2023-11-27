6fae35e43829f60a529f29c345871e3bce4b0814
bernardbdas <bernard.das@mca.christuniversity.in>
2023-11-27 23:37:11 +0530

    first commit

---
+++ src/main/java/io/jenkins/plugins/sample/SQA/TriggerPayload.java
@@ -0,0 +1,27 @@
+package io.jenkins.plugins.sample.SQA;
+
+public class TriggerPayload {
+    String token;
+
+    // Constructor
+    TriggerPayload(String token) {
+        this.token = token;
+    }
+
+    // Getters
+    public String getPayload() {
+        StringBuilder sb = new StringBuilder("");
+        sb.append("{\"token\":\"" + this.token + "\"}");
+
+        return sb.toString();
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    // Setters
+    protected void setToken(String token) {
+        this.token = token;
+    }
+}
